3ebd5f4a3f98f9eb6e1b5b48db9074127a331893
FallingSakura <dbw1732351264@163.com>
2024-12-26 23:35:23 +0800

    update title and add FontAwesome icons for social links; adjust styles and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import avatar from '/avatar.jpg'
 import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faBilibili,
+  faTwitter,
+  faGithub,
+  faWeixin,
+  faQq,
+} from '@fortawesome/free-brands-svg-icons'
 
 function App() {
   return (
@@ -10,11 +18,27 @@ function App() {
             <div className="img-container">
               <img src={avatar} alt="avatar" />
             </div>
-            <div className="title">
-              <h1>Hi, I'm <span className="name">FallingSakura</span>.</h1>
+            <div className="title flex">
+              <h1>
+                Hi, I'm <span className="name">FallingSakura</span>.
+              </h1>
             </div>
           </div>
-          <div className="social"></div>
+          <div className="social flex">
+            <div className="links flex">
+              <a href="https://space.bilibili.com/1722315602">
+                <FontAwesomeIcon icon={faBilibili} size="xl" />
+              </a>
+              <a href="https://github.com/FallingSakura">
+                <FontAwesomeIcon icon={faGithub} size="xl" />
+              </a>
+              <a href="https://x.com/SakuraFalling1">
+                <FontAwesomeIcon icon={faTwitter} size="xl" />
+              </a>
+              <FontAwesomeIcon icon={faWeixin} size="xl" />
+              <FontAwesomeIcon icon={faQq} size="xl" />
+            </div>
+          </div>
         </div>
         {/* <div className="test">
           <div className="box"></div>
