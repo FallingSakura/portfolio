bc5b20bd2077437abfb79328c0390c0a9d9ce275
FallingSakura <dbw1732351264@163.com>
2025-01-07 12:17:05 +0800

    style: 优化移动端导航栏样式

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -32,9 +32,11 @@ function MobileNav({
             }}
           >
             <a href={`#${link.id}`} onClick={toggle}>
-              {link.icon && <FontAwesomeIcon icon={link.icon} size="lg" />}
               <span>{link.title}</span>
             </a>
+            <div className="icon-container">
+              {link.icon && <FontAwesomeIcon icon={link.icon} size="lg" />}
+            </div>
           </li>
         ))}
       </ul>
