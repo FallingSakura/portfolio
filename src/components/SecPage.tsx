c3bfebe85bfd3fb0e4a47e52e8e457d06b8d930c
FallingSakura <dbw1732351264@163.com>
2025-01-07 15:01:16 +0800

    feat: initialize project card

--- src/components/SecPage.tsx
+++ src/components/SecPage.tsx
@@ -1,5 +1,13 @@
 import '../styles/SecPage.css'
 function SecPage() {
-  return <div className="sec" id="projects"></div>
+  return (
+    <div className="sec" id="projects">
+      <div className="card-container">
+        <div className="project-card">
+          <h3>Tech News</h3>
+        </div>
+      </div>
+    </div>
+  )
 }
 export default SecPage
