26548efc2e8466b5fb72a9a00293bb090303d267
FallingSakura <dbw1732351264@163.com>
2025-01-08 19:23:15 +0800

    feat: project card

--- src/components/SecPage.tsx
+++ src/components/SecPage.tsx
@@ -1,11 +1,15 @@
 import '../styles/SecPage.css'
+import { projects } from '../data/projects'
 function SecPage() {
   return (
     <div className="sec" id="projects">
       <div className="card-container">
-        <div className="project-card">
-          <h3>tech news</h3>
-        </div>
+        {projects.map((project) => (
+          <div className="project-card" key={project.title}>
+            <h3>{project.title}</h3>
+            <p>{project.descr}</p>
+          </div>
+        ))}
       </div>
     </div>
   )
---
+++ src/data/projects.ts
@@ -0,0 +1,22 @@
+export const projects = [
+  {
+    title: "",
+    descr: "",
+    url: "",
+    /* things below maybe use backend */
+    commits: 0,
+    stars: "",
+    forks: "",
+    languages: [""],
+  },
+  {
+    title: "Tech News",
+    descr: "A website for the latest tech news.",
+    url: "https://technews.fallingsakura.top",
+    commits: 0,
+    stars: "",
+    forks: "",
+    languages: [""],
+  }
+  /* idea: gird draggable */
+]
