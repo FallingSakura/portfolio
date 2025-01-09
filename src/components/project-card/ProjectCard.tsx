0543c8be369e1c164bcb6cecc5d52621f8e3186e
FallingSakura <dbw1732351264@163.com>
2025-01-09 19:32:22 +0800

    feat: project card hover animation

--- src/components/project-card/ProjectCard.tsx
+++ src/components/project-card/ProjectCard.tsx
@@ -1,22 +1,24 @@
 import '@/styles/project-card.css'
 import { Project } from '../../types/project'
 import LanguageLabel from './LanguageLabel'
-function ProjectCard({ title, descr, languages }: Project) {
+function ProjectCard({ title, descr, url, languages }: Project) {
   return (
     <div className="project-card">
-      <div className="content-container">
-        <div className="title">
-          <h3>{title}</h3>
+      <a href={url} target="_blank">
+        <div className="content-container">
+          <div className="title">
+            <h3>{title}</h3>
+          </div>
+          <div className="descr">
+            <p>{descr}</p>
+          </div>
+          <div className="footer">
+            {languages.map((language) => (
+              <LanguageLabel language={language} />
+            ))}
+          </div>
         </div>
-        <div className="descr">
-          <p>{descr}</p>
-        </div>
-        <div className="footer">
-          {languages.map((language) => (
-            <LanguageLabel language={language} />
-          ))}
-        </div>
-      </div>
+      </a>
     </div>
   )
 }
