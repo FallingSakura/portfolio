a4aa7e22f18ddec0b8a5bffec728d5597178d5ab
FallingSakura <dbw1732351264@163.com>
2025-01-13 08:23:26 +0800

    fix: relative toggle theme

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -8,17 +8,10 @@ function ToggleTheme({
   theme: boolean
   toggleTheme: () => void
 }) {
-  const isMobileView = window.innerWidth <= 768
   return (
     <div
       className="toggle-theme"
       onClick={toggleTheme}
-      style={{
-        left: `${isMobileView ? 'unset' : '0'}`,
-        right: `${isMobileView ? '0' : 'unset'}`,
-        top: `${isMobileView ? '0' : 'unset'}`,
-        bottom: `${isMobileView ? 'unset' : '0'}`
-      }}
     >
       <FontAwesomeIcon
         className={`icon ${theme ? 'visible' : 'hidden'}`}
