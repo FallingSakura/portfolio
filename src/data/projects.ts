e3e04da32b144b3c7193f64a82a886c973f55d75
FallingSakura <dbw1732351264@163.com>
2025-01-16 22:00:48 +0800

    style: perf the card-container

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -12,7 +12,7 @@ export const projects: Project[] = [
     title: "Tech News",
     descr: "Tech News, a website from our project team using next.js and flask api.",
     url: "https://technews.fallingsakura.top",
-    img: "/avatar.jpg",
+    img: "/project-images/technews.jpeg",
     commits: 0,
     stars: 0,
     forks: 0,
