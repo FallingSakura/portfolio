730965b1e5e0d81fc1659811b8bf709ea2735598
FallingSakura <dbw1732351264@163.com>
2025-01-17 15:39:29 +0800

    style: new gallery

--- src/components/ProjectPage.tsx
+++ src/components/ProjectPage.tsx
@@ -1,23 +1,58 @@
 import '../styles/project-page.css'
 import { projects } from '../data/projects'
 import ProjectCard from './project-card/ProjectCard'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
+import { useState, useRef, useEffect } from 'react'
+
+let container_width = 0
+let init = 0
 function ProjectPage() {
+  const [pos, setPos] = useState(0)
+  const card_container = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    if (card_container.current) {
+      container_width = card_container.current.offsetWidth
+    }
+  }, [])
+  useEffect(() => {
+    init = container_width / 2 - 200
+    setPos(init)
+  }, [container_width])
+  function scrollForward() {
+    if (pos === init - (projects.length - 1) * 375) {
+      return
+    }
+    setPos(pos - 375)
+  }
+  function scrollBack() {
+    if (pos === init) {
+      return
+    }
+    setPos(pos + 375)
+  }
   return (
     <div className="project" id="projects">
-      <div className="card-container">
-        {/* <span className="left-btn">
-          <FontAwesomeIcon icon={faAngleLeft} />
-        </span> */}
-        {projects.map((project) => (
-          <div className="card">
+      <div
+        className="card-container"
+        ref={card_container}
+        style={{
+          transform: `translateX(${pos}px)`
+        }}
+      >
+        {projects.map((project, index) => (
+          <div className="card" key={index}>
             <ProjectCard {...project} key={project.title} />
           </div>
         ))}
-        {/* <span className="right-btn">
+      </div>
+      <div className="controller">
+        <span className="left-btn" onClick={scrollBack}>
+          <FontAwesomeIcon icon={faAngleLeft} />
+        </span>
+        <span className="right-btn" onClick={scrollForward}>
           <FontAwesomeIcon icon={faAngleRight} />
-        </span> */}
+        </span>
       </div>
     </div>
   )
