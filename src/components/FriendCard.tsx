c6f8ae346807bb262e1c7d2e77e2f184c0ceb75b
FallingSakura <dbw1732351264@163.com>
2025-01-18 18:18:27 +0800

    feat: initialize friend card

---
+++ src/components/FriendCard.tsx
@@ -0,0 +1,13 @@
+import '@/styles/friend-card.css'
+function FriendCard() {
+  return (
+    <div className="friend-card">
+      <div className="card-container">
+      <div className="avatar"></div>
+        <h3>FallingSakura</h3>
+      </div>
+    </div>
+  )
+}
+
+export default FriendCard
--- src/components/FriendPage.tsx
+++ src/components/FriendPage.tsx
@@ -1,9 +1,10 @@
 import '@/styles/friend-page.css'
+import FriendCard from './FriendCard'
 function FriendPage() {
   return (
     <div className="friend" id="friend">
       <div className="container">
-
+        <FriendCard />
       </div>
     </div>
   )
