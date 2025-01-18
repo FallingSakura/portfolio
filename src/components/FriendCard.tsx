1cbd83be49c22e15ff43bfe7f6288b77e0bc8cda
FallingSakura <dbw1732351264@163.com>
2025-01-18 19:24:02 +0800

    feat: add 'said' to our friends' card

--- src/components/FriendCard.tsx
+++ src/components/FriendCard.tsx
@@ -1,18 +1,51 @@
 import '@/styles/friend-card.css'
-import { Friend } from '../types/friend' 
-function FriendCard({ name, avatar, link, said, index }: (Friend & {index:number})) {
+import { Friend } from '../types/friend'
+import { useRef, useState, useEffect } from 'react'
+function FriendCard({
+  name,
+  avatar,
+  link,
+  said,
+  index
+}: Friend & { index: number }) {
+  const title = useRef<HTMLHeadingElement>(null)
+  const text = useRef<HTMLParagraphElement>(null)
+  const card_container = useRef<HTMLAnchorElement>(null)
+  const [hover, setHover] = useState(false)
   const prop = index
-  console.log(prop)
+  useEffect(() => {
+    if (title.current && text.current) {
+      const title_width = `${title.current.offsetWidth.toString()}px`
+      text.current.style.maxWidth = title_width
+    }
+  }, [])
+  useEffect(() => {
+    if (card_container.current && text.current) {
+      const pH = text.current.offsetHeight
+      card_container.current.style.setProperty('--leng-pb', `${pH + 25}px`)
+    }
+  }, [hover])
   return (
-    <div className="friend-card">
-      <a className="card-container" href={link}>
-        <div className="avatar" style={{
-          backgroundImage: `url(${avatar})`
-        }}></div>
-        <h3>{name}</h3>
-        <p>{said}</p>
-      </a>
-    </div>
+    <>
+      <div className="friend-card">
+        <a
+          className="card-container"
+          href={link}
+          ref={card_container}
+          onMouseOver={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+        >
+          <div
+            className="avatar"
+            style={{
+              backgroundImage: `url(${avatar})`
+            }}
+          ></div>
+          <h3 ref={title}>{name}</h3>
+        </a>
+        <span ref={text}>{said}</span>
+      </div>
+    </>
   )
 }
 
--- src/components/FriendPage.tsx
+++ src/components/FriendPage.tsx
@@ -6,7 +6,7 @@ function FriendPage() {
     <div className="friend" id="friend">
       <div className="container">
         {friends.map((friend, index) => (
-          <FriendCard {...friend} index={index} />
+          <FriendCard {...friend} index={index} key={friend.name} />
         ))}
       </div>
     </div>
