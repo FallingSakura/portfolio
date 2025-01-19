a5a957335a782cb2b74e0a926c9ff78b9f5e5dbf
FallingSakura <dbw1732351264@163.com>
2025-01-19 09:48:39 +0800

    fix: fix mobile error classname

--- src/components/FriendCard.tsx
+++ src/components/FriendCard.tsx
@@ -1,6 +1,6 @@
 import '@/styles/friend-card.css'
 import { Friend } from '../types/friend'
-import { useRef, useState, useEffect } from 'react'
+import { useRef, useEffect } from 'react'
 function FriendCard({
   name,
   avatar,
@@ -11,7 +11,6 @@ function FriendCard({
   const title = useRef<HTMLHeadingElement>(null)
   const text = useRef<HTMLParagraphElement>(null)
   const card_container = useRef<HTMLAnchorElement>(null)
-  const [hover, setHover] = useState(false)
   const param = index
   console.log(param)
   useEffect(() => {
@@ -19,22 +18,19 @@ function FriendCard({
       const title_width = `${title.current.offsetWidth.toString()}px`
       text.current.style.maxWidth = title_width
     }
-  }, [])
-  useEffect(() => {
     if (card_container.current && text.current) {
       const pH = text.current.offsetHeight
       card_container.current.style.setProperty('--leng-pb', `${pH + 25}px`)
     }
-  }, [hover])
+  }, [])
   return (
     <>
       <div className="friend-card">
         <a
           className="card-container"
           href={link}
+          target="_blank"
           ref={card_container}
-          onMouseOver={() => setHover(true)}
-          onMouseLeave={() => setHover(false)}
         >
           <div
             className="avatar"
