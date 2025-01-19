ca7ca1060f4475983d3791147d31ecb176603a34
FallingSakura <dbw1732351264@163.com>
2025-01-19 14:50:31 +0800

    feat: card-draggable

--- src/components/FriendCard.tsx
+++ src/components/FriendCard.tsx
@@ -1,15 +1,14 @@
 import '@/styles/friend-card.css'
 import { Friend } from '../types/friend'
-import { useRef, useEffect } from 'react'
-function FriendCard({
-  name,
-  avatar,
-  link,
-  said,
-}: Friend) {
+import { useRef, useEffect, forwardRef } from 'react'
+import Draggable from 'react-draggable'
+function FriendCard(
+  { name, avatar, link, said }: Friend,
+  card_ref: React.ForwardedRef<HTMLDivElement>
+) {
   const title = useRef<HTMLHeadingElement>(null)
   const text = useRef<HTMLParagraphElement>(null)
-  const card_container = useRef<HTMLAnchorElement>(null)
+  const card_container = useRef<HTMLDivElement>(null)
   useEffect(() => {
     if (title.current && text.current) {
       const title_width = `${(title.current.offsetWidth - 5).toString()}px`
@@ -17,30 +16,28 @@ function FriendCard({
     }
     if (card_container.current && text.current) {
       const pH = text.current.offsetHeight
+      console.log(pH)
       card_container.current.style.setProperty('--leng-pb', `${pH + 26}px`)
     }
   }, [])
   return (
-    <>
-      <div className="friend-card">
-        <a
-          className="friend-card-container"
-          href={link}
-          target="_blank"
-          ref={card_container}
-        >
+    <Draggable>
+      <div className="friend-card" ref={card_ref}>
+        <div className="friend-card-container" ref={card_container}>
           <div
             className="avatar"
             style={{
               backgroundImage: `url(${avatar})`
             }}
           ></div>
-          <h3 ref={title}>{name}</h3>
-        </a>
+          <a href={link} target="_blank">
+            <h3 ref={title}>{name}</h3>
+          </a>
+        </div>
         <span ref={text}>{said}</span>
       </div>
-    </>
+    </Draggable>
   )
 }
 
-export default FriendCard
+export default forwardRef<HTMLDivElement, Friend>(FriendCard)
--- src/components/FriendPage.tsx
+++ src/components/FriendPage.tsx
@@ -1,12 +1,19 @@
 import '@/styles/friend-page.css'
+import { useRef } from 'react'
 import FriendCard from './FriendCard'
 import { friends } from '../data/friends'
 function FriendPage() {
+  const refs = useRef<HTMLDivElement[]>([])
+  const setRef = (index: number, element: HTMLDivElement | null) => {
+    if (element) {
+      refs.current[index] = element
+    }
+  }
   return (
     <div className="friend" id="friend">
       <div className="friend-cards-container">
         {friends.map((friend, index) => (
-          <FriendCard {...friend} index={index} key={friend.name} />
+          <FriendCard {...friend} ref={(el) => setRef(index, el)} key={friend.name} />
         ))}
       </div>
     </div>
