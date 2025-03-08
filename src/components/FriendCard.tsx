42a7294d4a2d228aeaebc038f2bbe06fd2180a89
FallingSakura <dbw1732351264@163.com>
2025-03-08 12:18:28 +0800

    refactor: update FriendCard component for improved ref handling and responsiveness; adjust styles for better visual consistency

--- src/components/FriendCard.tsx
+++ src/components/FriendCard.tsx
@@ -1,16 +1,21 @@
 import '@/styles/friend-card.css'
 import { Friend } from '../types/friend'
 import { useRef, useEffect, useState, forwardRef } from 'react'
-function FriendCard(
-  props: Friend,
-  card_ref: React.ForwardedRef<HTMLDivElement>
-) {
+function FriendCard(props: Friend, ref: React.ForwardedRef<HTMLDivElement>) {
   const { name, avatar, link, said } = props
   const title = useRef<HTMLHeadingElement>(null)
   const text = useRef<HTMLParagraphElement>(null)
   const card_container = useRef<HTMLDivElement>(null)
-  const cardRef = useRef<HTMLDivElement>(null)
+  const cardRef = useRef<HTMLDivElement | null>(null)
   const [isActive, setIsActive] = useState(false)
+  const setRef = (el: HTMLDivElement) => {
+    cardRef.current = el
+    if (typeof ref === 'function') {
+      ref(el)
+    } else if (ref) {
+      ref.current = el
+    }
+  }
   useEffect(() => {
     if (title.current && text.current) {
       const title_width = `${(title.current.offsetWidth - 3).toString()}px`
@@ -23,22 +28,24 @@ function FriendCard(
   }, [])
   const activate = () => {
     if (isActive) return
-    console.log(typeof card_ref)
+    if (cardRef.current) {
+      cardRef.current.style.zIndex = '10'
+    }
     setIsActive(true)
   }
   const deactivate = () => {
     if (!isActive) return
     setIsActive(false)
-    // setTimeout(() => {
-    //   if (typeof card_ref !== 'function' && card_ref?.current) {
-    //     card_ref.current.style.zIndex = '0'
-    //   }
-    // }, 500)
+    setTimeout(() => {
+      if (cardRef.current) {
+        cardRef.current.style.zIndex = '1'
+      }
+    }, 500)
   }
   return (
     <div
       className={`friend-card ${isActive ? 'active' : ''}`}
-      ref={card_ref}
+      ref={setRef}
       // onClick={activate}
       onMouseOver={activate}
       onMouseLeave={deactivate}
