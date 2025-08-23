4b6d90be0d15510e0232c79acf130ace57c45662
FallingSakura <dbw1732351264@163.com>
2025-08-23 14:44:44 +0800

    refactor: restructure Essay component layout and update associated styles

--- src/components/Essay.tsx
+++ src/components/Essay.tsx
@@ -3,16 +3,20 @@ const Essay = () => {
   return (
     <div id="essay" className="essay">
       <div className="essay-content">
-        <header className="card">
-          <h2>Essay</h2>
-          <p>
-            I am a student of Computer Science and Engineering at the University
-            of California, San Diego. I am interested in machine learning and
-            artificial intelligence. Lorem, ipsum dolor sit amet consectetur
-            adipisicing elit. Animi at consequatur ratione. Voluptates magnam
-            blanditiis placeat corporis dolorem possimus minus, officia
-            incidunt. Eos ad at voluptas ratione aperiam, ipsa consequatur?
-          </p>
+        <header>
+          <div className="descr card">
+            <h2>Essay</h2>
+            <p>
+              I am a student of Computer Science and Engineering at the
+              University of California, San Diego. I am interested in machine
+              learning and artificial intelligence. Lorem, ipsum dolor sit amet
+              consectetur adipisicing elit. Animi at consequatur ratione.
+              Voluptates magnam blanditiis placeat corporis dolorem possimus
+              minus, officia incidunt. Eos ad at voluptas ratione aperiam, ipsa
+              consequatur?
+            </p>
+            <div className="card copilot-button"></div>
+          </div>
         </header>
         <main>
           <div className="card"></div>
